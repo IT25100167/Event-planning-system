4ad3c690194ec70c2c21e2bb76be57289160699c
Chethana WNP <it25100164@mysliit.lk>
2026-08-31 02:32:08 +0530

    Add email existence check to UserRepository

--- backend/src/main/java/edu/sliit/repository/UserRepository.java
+++ backend/src/main/java/edu/sliit/repository/UserRepository.java
@@ -4,5 +4,6 @@ import edu.sliit.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
     boolean existsByEmail(String email);
 }
